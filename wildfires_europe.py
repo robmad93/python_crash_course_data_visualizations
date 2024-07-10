95d3794432f9cda904d8680a7b885b5b3797d76a
Robert Madigan <131868277+robmad93@users.noreply.github.com>
2024-07-10 17:19:50 +0100

    Included wildfire intensity data for Europe in first week of July, 2024.

---
+++ wildfires_europe.py
@@ -0,0 +1,30 @@
+import csv
+from datetime import datetime
+import os
+import matplotlib.pyplot as plt
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
+import pandas as pd
+import plotly.express as px
+
+os.chdir("C:/Users/robma/Desktop/Python/data_visualization")  # Change to file path.
+
+filename = "data/MODIS_C6_1_Europe_7d.csv"
+
+# Read CSV file into a pandas DataFrame
+df = pd.read_csv(filename)
+
+# Map the wildfires using Plotly Express
+fig = px.density_mapbox(
+    df,
+    lat="latitude",
+    lon="longitude",
+    z="frp",  # Assuming "frp" is the column name for fire radiative power
+    radius=20,
+    center=dict(lat=42.83, lon=-8.35),
+    zoom=8,
+    mapbox_style="open-street-map",
+    title="European wildfires - July, 2024",
+)
+
+fig.show()
